61fccf6226175f3ed20cde37471a412df14ee86b
m1nsk <omicron123>
2017-04-17 17:13:51 +0400

    issue 1 start

---
+++ rsqm/apps/supplier/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Warehouse, Email, Supplier
+
+admin.site.register(Warehouse)
+admin.site.register(Email)
+admin.site.register(Supplier)
+
+# Register your models here.
