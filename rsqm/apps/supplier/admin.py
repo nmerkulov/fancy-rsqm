994ef735efb63463952738ecad49ef2d39185d92
m1nsk <omicron123>
2017-04-17 18:46:42 +0400

    issue 1 start

--- rsqm/apps/supplier/admin.py
+++ rsqm/apps/supplier/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Warehouse, Email, Supplier
 
+
+class EmailInline(admin.StackedInline):
+    model = Email
+
+
+class SupplierAdmin(admin.ModelAdmin):
+    inlines = [
+        EmailInline,
+    ]
+
 admin.site.register(Warehouse)
 admin.site.register(Email)
-admin.site.register(Supplier)
-
+admin.site.register(Supplier, SupplierAdmin)
 # Register your models here.
