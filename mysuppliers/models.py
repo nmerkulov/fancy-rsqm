a22f1339425974a9c36ff7a52e5ad7a3de018c1a
m1nsk <omicron123>
2017-04-17 14:31:50 +0400

    issue 1 start

---
+++ mysuppliers/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Email(models.Model):
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=30)
+    warehouse = models.ManyToManyField(Warehouse)
+    email = models.ManyToManyField(Email)
+
+    def __str__(self):
+        return self.name
+
+
+
