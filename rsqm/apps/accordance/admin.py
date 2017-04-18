76e0f05fe3117fb8fb903b68af30f9943247a14e
Balykovsky <abalykovskiy@gmail.com>
2017-04-18 21:06:43 +0300

    add some admin models

--- rsqm/apps/accordance/admin.py
+++ rsqm/apps/accordance/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Product, Match, Quantity
 
 
+class QuantityAdmin(admin.ModelAdmin):
+    list_display = ['product', 'warehouse', 'quantity']
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ['product', 'supplier_code', 'supplier']
+
 admin.site.register(Product)
-admin.site.register(Match)
-admin.site.register(Quantity)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(Quantity, QuantityAdmin)
--- rsqm/apps/accordance/models.py
+++ rsqm/apps/accordance/models.py
@@ -5,6 +5,9 @@ from apps.supplier.models import Supplier, Warehouse
 class Product(models.Model):
     code = models.IntegerField()
 
+    def __str__(self):
+        return str(self.code)
+
 
 class Match(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -12,7 +15,7 @@ class Match(models.Model):
     supplier_code = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.suplier_code
+        return self.supplier_code
 
 
 class Quantity(models.Model):
