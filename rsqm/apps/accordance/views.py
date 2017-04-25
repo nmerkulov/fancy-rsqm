57cb67bffc3c216d7f2940dbf6bf8175299f8397
m1nsk <omicron123>
2017-04-25 16:26:41 +0400

    issue 7 start

--- rsqm/apps/accordance/views.py
+++ rsqm/apps/accordance/views.py
@@ -6,52 +6,6 @@ from django.core.exceptions import ObjectDoesNotExist
 import xlrd
 import xlwt
 
-def test(request):
-    path = 'C:/Users/mnk2/my-project/fancy-rsqm/example.xls'
-    try:
-        a = xlrd.open_workbook(path)
-    except IOError:
-        generatexls(request, path)
-        a = xlrd.open_workbook(path)
-    sheet = a.sheet_by_index(0)
-    row = []
-    message = []
-    try:
-        supplier = Supplier.objects.get(name='supplier 1')
-    except ObjectDoesNotExist:
-        initdb(request, size=3)
-
-    for rownum in range(sheet.nrows):
-        row.append(sheet.row_values(rownum))
-    if row[0] == ['Product', 'Quantity', 'Warehouse']:
-        print(row, 'row')
-        row = row[1:]
-        print(row, 'row')
-        for item in row:
-            print('in row', len(row))
-            try:
-                print('warehouse')
-                wareh = Warehouse.objects.get(name=str(item[2]))
-                try:
-                    print('product')
-                    prod = Product.objects.get(code=int(item[0]))
-                    try:
-                        qty = Quantity(warehouse=wareh, product=prod)
-                        qty.quantity = int(item[1])
-                        qty.save()
-                        message.append(qty.quantity)
-                    except ObjectDoesNotExist:
-                        Quantity.objects.create(werehouse=wareh, prduct=prod, quantity=int(item[1]))
-                        message.append('new entry', item)
-                except ObjectDoesNotExist:
-                    print(int(item[0]))
-                    message.append('no such product in base ' + str(int(item[0])) + ' ')
-            except ObjectDoesNotExist:
-                message.append('unknown warehouse, check your input doc' + str(item[2] + ' '))
-
-    return HttpResponse(''.join(message))
-
-
 def initdb(request, size=3):
 
     def testinitdb(num):
@@ -69,28 +23,6 @@ def initdb(request, size=3):
         testinitdb(i)
 
     return HttpResponse('db init success')
-
-
-def generatexls(request, path = 'example.xls'):
-
-    def fillxlrow(rownum, table):
-        rownum = int(rownum)
-        table.write(rownum + 1, 0, 1001 * (i + 1))
-        table.write(rownum + 1, 1, 11 * (i + 1))
-        table.write(rownum + 1, 2, 'warehouse ' + str(rownum))
-
-    xl = xlwt.Workbook()
-    xls = xl.add_sheet('supplier rest')
-    col = ['Product', 'Quantity', 'Warehouse']
-    for index, colname in enumerate(col):
-        xls.write(0, index, colname)
-
-    for i in range(3):
-        fillxlrow(i, xls)
-
-    print(xl.save(path))
-
-    return HttpResponse('generated xls')
 
 
 def supplier_list(request):
