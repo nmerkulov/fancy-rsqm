daf01d2637b093da06a0d89f7684a003df93ddb4
m1nsk <omicron123>
2017-04-25 17:48:26 +0400

    issue 7 start

--- rsqm/apps/accordance/urls.py
+++ rsqm/apps/accordance/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from .views import test, initdb, generatexls, supplier_list, upload_quantity
+from .views import supplier_list, upload_quantity
 
 urlpatterns = [
-    url(r'^(\d+)/$', upload_quantity),
+    url(r'^(?P<supplier_id>\d+)/$', upload_quantity),
     url(r'^', supplier_list),
 ]
