ae4a5deaf7ecf93344dc19988ce778673ed00325
Balykovsky <abalykovskiy@gmail.com>
2017-04-29 18:22:38 +0300

    add fixes to urls reversing

--- rsqm/apps/supplier/urls.py
+++ rsqm/apps/supplier/urls.py
@@ -10,7 +10,7 @@ from apps.supplier.views import (edit_supplier_card,
 urlpatterns = [
     url(r'^$', SupplierListView.as_view(), name='sup_list'),
     url(r'^(?P<pk>\d+)/$', SupplierDetailView.as_view()),
-    url(r'^add/$', add_supplier_card),
-    url(r'^(?P<s_id>\d+)/edit/$', edit_supplier_card),
-    url(r'^(?P<s_id>\d+)/delete/$', delete_supplier_card),
+    url(r'^add/$', add_supplier_card, name='add_card'),
+    url(r'^(?P<s_id>\d+)/edit/$', edit_supplier_card, name='edit_card'),
+    url(r'^(?P<s_id>\d+)/delete/$', delete_supplier_card, name='delete_card'),
     ]
